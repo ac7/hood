b8e67e76c7dcad706fc9d131cbb06608993e79c9
ac7 <ac7@users.noreply.github.com>
2014-05-16 21:26:00 -0700

    Remove pulsing effect on archers.

--- archer.lua
+++ archer.lua
@@ -4,8 +4,6 @@ local Human = require "human"
 local Arrow = require "arrow"
 
 local Archer = Human:extends{
-	y_offset = 0,
-	pulse = 0,
 	speed = 256,
 	name = "Archer",
 }
@@ -17,15 +15,6 @@ function Archer:__init()
 end
 
 function Archer:update(dt)
-	self.pulse = self.pulse + dt * 3
-	self.y_offset = math.sin(self.pulse) * 8
-end
-
-function Archer:draw(offset_x, offset_y)
-	local old_y = self.y
-	self.y = self.y + self.y_offset
-	Archer.super.draw(self, offset_x, offset_y)
-	self.y = old_y
 end
 
 function Archer:shoot(target_x, target_y)
