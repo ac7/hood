52838e5a744505bfb18d8a272c4a02d41e9d28ca
ac7 <ac7@users.noreply.github.com>
2014-05-17 11:55:35 -0700

    Actor depth sort.

--- state.lua
+++ state.lua
@@ -16,11 +16,12 @@ function State:update(dt)
 	b = b + (self.bg_color[3] - b) * dt * transition_speed
 	love.graphics.setBackgroundColor(r, g, b)
 
-	for i, v in pairs(self.actors) do
+	for i = #self.actors, 1, -1 do
+		local v = self.actors[i]
 		assert(v)
 		assert(v.update)
 		if not v.active then
-			self.actors[i] = nil
+			table.remove(self.actors, i)
 		else
 			v:update(dt)
 		end
@@ -30,6 +31,12 @@ end
 function State:draw()
 	assert(self.offset_x)
 	assert(self.offset_y)
+	table.sort(self.actors, function(a1, a2)
+		if a1 and a2 and a1.y and a2.y then
+			return a1.y < a2.y
+		end
+		return false
+	end)
 	for _, v in pairs(self.actors) do
 		assert(v)
 		assert(v.draw)
