ad3c5d8e533ac4f5aba807d2a6ed938c6fa5265f
ac7 <ac7@users.noreply.github.com>
2014-05-17 01:08:56 -0700

    Much improved AI algorithm.
    The algorithm now has two modes of operation: wander and hostile. I made
    the decision to use these two modes instead of my original idea of three
    (wander, ally, and hostile) because I realized that the factions would
    provide enough information for an NPC to decide whether it was friendly or
    not.
    
    Accordingly, NPCs will not attack once set to "wander" mode. If left alone
    while in hostile mode, they will revert back to wandering mode. While in
    hostile mode, they will move frequently and shoot towards the nearest
    enemy.

--- npc.lua
+++ npc.lua
@@ -3,37 +3,77 @@ local Archer = require "archer"
 local util = require "util"
 
 local NPC = Archer:extends{
-	strategy = "neutral",
-	wander_point_x = 0.0,
-	wander_point_y = 0.0,
+	strategy = "wander",
+	wander_point_x = nil,
+	wander_point_y = nil,
 	wander_distance = 512,
-	speed = 64,
+	speed = 96,
 }
+
+function NPC:__init(strategy, faction)
+	NPC.super.__init(self)
+	assert(type(faction) == "number")
+	assert(type(strategy) == "string")
+	self.faction = faction
+	self.strategy = strategy
+end
 
 function NPC:update(dt)
 	local choose_new_point = false
-	if util.dist(self.x, self.y, self.wander_point_x, self.wander_point_y)
-	< (self.width + self.height) / 2 then
+	if not self.wander_point_x or not self.wander_point_y then
 		choose_new_point = true
 	else
-		if self.wander_point_x < self.x then
-			choose_new_point = not self:move(-self.speed*dt, 0)
+		if util.dist(self.x, self.y, self.wander_point_x, self.wander_point_y) < (self.width + self.height) / 3 then
+			choose_new_point = true
 		else
-			choose_new_point = not self:move(self.speed*dt, 0)
-		end
+			local path_clear
+			if self.wander_point_x < self.x then
+				path_clear = self:move(-self.speed*dt, 0)
+			else
+				path_clear = self:move(self.speed*dt, 0)
+			end
 
-		if self.wander_point_y < self.y then
-			choose_new_point = not self:move(0, -self.speed*dt)
-		else
-			choose_new_point = not self:move(0, self.speed*dt)
+			if self.wander_point_y < self.y then
+				path_clear = self:move(0, -self.speed*dt)
+			else
+				path_clear = self:move(0, self.speed*dt)
+			end
+
+			if not path_clear then
+				self.wander_point_x = self.x + (math.random() - 0.5) * self.wander_distance
+				self.wander_point_y = self.y + (math.random() - 0.5) * self.wander_distance
+			end
 		end
 	end
 
-	if choose_new_point then
-		if self.strategy == "neutral" then
+	if choose_new_point == true then
+		if self.strategy == "wander" then
 			self.wander_point_x = self.x + (math.random() - 0.5) * self.wander_distance
 			self.wander_point_y = self.y + (math.random() - 0.5) * self.wander_distance
 		elseif self.strategy == "hostile" then
+			local closest_distance = nil
+			local closest_actor = nil
+
+			for _, actor in pairs(state.actors) do
+				local actor_dist = util.dist(actor.x, actor.y, self.x, self.y)
+				if actor ~= self and actor.get_faction and (not closest_distance or actor_dist < closest_distance)
+				and actor:get_faction() ~= self:get_faction() then
+					closest_distance = actor_dist
+					closest_actor = actor
+				end
+			end
+
+			if closest_actor == nil then
+				self.strategy = "wander"
+			else
+				if closest_actor:get_faction() ~= self:get_faction() then
+					self:shoot(closest_actor.x, closest_actor.y)
+					self.wander_point_x = self.x + (math.random() - 0.5) * self.wander_distance*2/3
+					self.wander_point_y = self.y + (math.random() - 0.5) * self.wander_distance*2/3
+				end
+			end
+		else
+			error("Invalid strategy " .. tostring(self.strategy))
 		end
 	end
 end
