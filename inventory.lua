a23d3e5181e85bcb25e9e4d58e7e3ac53998539a
ac7 <ac7@users.noreply.github.com>
2014-05-18 14:52:50 -0500

    Inventory system. Resolve #3
    No category for money exists yet because I haven't decided how that will
    be implemented in the game.

---
+++ inventory.lua
@@ -0,0 +1,55 @@
+
+local State = require "state"
+local Human = require "human"
+local Actor = require "actor"
+local MenuItem = require "menu_item"
+
+local Inventory = State:extends()
+
+function Inventory:__init()
+	Inventory.super.__init(self)
+	self.bg_color = {42, 32, 22}
+end
+
+function Inventory:start()
+	self.actors = {}
+	local player = states.play.player
+	assert(player and player:is(Human))
+	local count = #player.inventory
+	for x = 1, count do
+		local item = player.inventory[x]
+		assert(item)
+
+		local xpos = x * (width / (count+1))
+		local repr = MenuItem(item, xpos)
+		table.insert(self.actors, repr)
+	end
+end
+
+function Inventory:keyreleased(key, unicode)
+	if key == "q" then
+		set_state(states.play)
+	end
+end
+
+function Inventory:mousereleased(mx, my, button)
+	for _, v in pairs(self.actors) do
+		if util.dist(mx, my, v.x, v.y) < (v.width + v.height) / 2 and v.item.holder then
+			local player = states.play.player
+			assert(player and player:is(Human))
+			player:drop(v.item)
+			break
+		end
+	end
+end
+
+function Inventory:draw()
+	Inventory.super.draw(self)
+	love.graphics.setColor(255, 255, 255)
+	love.graphics.rectangle("line", 32, 32, width - 64, height - 64)
+	love.graphics.setFont(fonts.medium)
+	love.graphics.printf("Inventory (" .. tostring(#self.actors) .. " items)", 0, 36, width, "center")
+end
+
+return Inventory
+
---
+++ menu_item.lua
@@ -0,0 +1,30 @@
+
+local Actor = require "actor"
+local Item = require "item"
+
+local MenuItem = Actor:extends()
+
+function MenuItem:__init(item, xpos)
+	assert(item and item:is(Item))
+	MenuItem.super.__init(self, item.image)
+	assert(type(xpos) == "number")
+	self.x = xpos
+	self.y = height / 2
+	self.item = item
+end
+
+function MenuItem:draw()
+	if self.item.holder then
+		love.graphics.setColor(255, 255, 255)
+		local scale = 1/self.item.width * (width / 8)
+		love.graphics.draw(self.item.image, self.x, self.y, 0, scale, scale, self.item.width/2, self.item.height/2)
+		assert(type(self.item.name) == "string")
+		love.graphics.printf(self.item.name, self.x - 200, self.y, 400, "center")
+	else
+		love.graphics.setFont(fonts.medium)
+		love.graphics.printf("Dropped", self.x - 200, self.y, 400, "center")
+	end
+end
+
+return MenuItem
+
