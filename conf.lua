402de9f596c5092a38569bbc97458e2d038d0414
ac7 <ac7@users.noreply.github.com>
2014-05-18 15:10:55 -0500

    Higher default resolution.

--- conf.lua
+++ conf.lua
@@ -4,9 +4,16 @@ function love.conf(t)
 	t.console = false
 
 	t.window.title = "robinhood"
-	t.window.width = 960
-	t.window.height = 540
+	t.window.width = 1024
+	t.window.height = 576
 	t.window.resizable = true
+	--[[
+
+	-- uncomment for fullscreen
+
+		t.window.fullscreentype = "desktop"
+		t.window.fullscreen = true
+	]]
 
 	t.modules.physics = false
 	t.modules.joystick = false
